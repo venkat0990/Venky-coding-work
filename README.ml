16d16476f731b301566d85c39065c3f884777b0e
venkat0990 <62494462+venkat0990@users.noreply.github.com>
2020-03-29 01:32:54 -0500

    Update README.ml

--- README.ml
+++ README.ml
@@ -1,2 +1,12 @@
-# Venky-coding-work
-I have joined Github to learn coding and get job opportunities
+This is the phone call audio processing, done using python to do start and stop function. Relevant libraries required are given below
+
+Installation of thinkdsp can be done using pip install thinkx
+
+from __future__ import print_function, division
+import thinkdsp
+import thinkplot
+import numpy 
+
+Signal is a mathematical entity which represents something that changes with time. Wave is a discrete version of a signal. Wave can move back and forth between time and frequency domain
+
+My latest commit is "Phone call audio sound processing using python.ipynb" which used files "music.wav" and "music1.wav" Please find these files attached in the latest commit
